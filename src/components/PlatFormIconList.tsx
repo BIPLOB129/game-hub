41cec48631c365ce4af5111846e45d07978e6e20
Biplob Talukdar <biplobtalukdar33@gmail.com>
2023-04-22 23:14:42 +0600

    Added platform icons

---
+++ src/components/PlatFormIconList.tsx
@@ -0,0 +1,39 @@
+import { PlatForm } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaLinux,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+interface Props {
+  platforms: PlatForm[];
+}
+const PlatFormIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatFormIconList;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "06c2754f50fb4558944baee6ab6bb98c",
+    key: "b783a2341716435a8453285425785090",
   },
 });
