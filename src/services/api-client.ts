aee7a1bbf3ff7df1cd85a8c6f68c6611bc4c3a29
Biplob Talukdar <biplobtalukdar33@gmail.com>
2023-04-22 21:00:29 +0600

    created a custom hook for fetching games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "2cef70833c2f46dba26850e8c7d464cf",
+    key: "372708e7d75943a2b357b5f4ac46f81c",
   },
 });
