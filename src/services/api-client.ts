16f376303946808a034f62280f863165a5f85479
Biplob Talukdar <biplobtalukdar33@gmail.com>
2023-04-22 21:25:06 +0600

    created a game card

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "372708e7d75943a2b357b5f4ac46f81c",
+    key: "06c2754f50fb4558944baee6ab6bb98c",
   },
 });
