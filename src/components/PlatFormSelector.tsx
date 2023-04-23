ff3708ea53655feb74a8270e46363160d16f4b1e
Biplob Talukdar <biplobtalukdar33@gmail.com>
2023-04-23 13:24:03 +0600

    build platform filtering

--- src/components/PlatFormSelector.tsx
+++ src/components/PlatFormSelector.tsx
@@ -1,19 +1,25 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
-
-const PlatFormSelector = () => {
+import { PlatForm } from "../hooks/useGames";
+interface Props {
+  onSelect: (platform: PlatForm) => void;
+  selectedPlatform: PlatForm | null;
+}
+const PlatFormSelector = ({ onSelect, selectedPlatform }: Props) => {
   const { data } = usePlatforms();
   return (
     <>
       <Menu>
         <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-          Platforms
+          {selectedPlatform?.name || "Platform"}
         </MenuButton>
         <MenuList>
           {" "}
           {data.map((platform) => (
-            <MenuItem key={platform.id}>{platform.name}</MenuItem>
+            <MenuItem onClick={() => onSelect(platform)} key={platform.id}>
+              {platform.name}
+            </MenuItem>
           ))}
         </MenuList>
       </Menu>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "a3282387f0e346e3bc139aea717bf7a0",
+    key: "68291e94a1a24e6584b145e96331fd4c",
   },
 });
