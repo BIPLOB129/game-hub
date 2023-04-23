eb72c10f166ed17b6a7a7e92e45d97146a79bc3e
Biplob Talukdar <biplobtalukdar33@gmail.com>
2023-04-23 23:45:08 +0600

    clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -18,11 +19,13 @@ const GenreList = ({ onSelect, selectedGenre }: Props) => {
   if (isLoading) return <Spinner />;
   return (
     <>
+      <Heading fontSize="2xl">Genres</Heading>
       <List>
         {data.map((data) => (
           <ListItem key={data.id} paddingY="5px">
             <HStack>
               <Image
+                objectFit="cover"
                 src={getOptimizedImage(data.image_background)}
                 boxSize="32px"
                 borderRadius={8}
@@ -31,6 +34,8 @@ const GenreList = ({ onSelect, selectedGenre }: Props) => {
                 variant="link"
                 onClick={() => onSelect(data)}
                 fontSize="lg"
+                whiteSpace="normal"
+                textAlign="left"
                 fontWeight={data.id === selectedGenre?.id ? "bold" : "normal"}
               >
                 {data.name}
